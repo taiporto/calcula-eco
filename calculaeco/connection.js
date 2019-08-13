ef9f1e279629008b6ec3d1a964cb84d258ca3804
Moon Koala <moonkoala23@gmail.com>
2019-08-13 12:00:11 -0300

    Database conn changes

--- calculaeco/connection.js
+++ calculaeco/connection.js
@@ -2,23 +2,30 @@ module.exports = {
     connect: function () {
         
         const MongoClient = require("mongodb").MongoClient;
-        // const mongoose = require('mongoose');
+        const mongoose = require('mongoose');
         const ObjectId = require("mongodb").ObjectID;
         
         // let CONNECTION_URL = "mongodb+srv://public:HxnpHEg3OnxJgDSt@cluster0-yfisq.gcp.mongodb.net/test?retryWrites=true&w=majority";
         let CONNECTION_URL = process.env.MONGODB_URI || "mongodb+srv://public:HxnpHEg3OnxJgDSt@cluster0-yfisq.gcp.mongodb.net/test?retryWrites=true&w=majority";
         const DATABASE_NAME = "bdcalculaeco";
 
-        MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        // MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
 
+        //     if (error) {
+        //         throw error;
+        //     }
+
+        //     database = client.db(DATABASE_NAME);
+
+        //     collection = database.collection("disciplinas");
+
+        //     console.log("Connected to `" + DATABASE_NAME + "`!");
+        // });
+
+        mongoose.connect(CONNECTION_URL, {dbName: DATABASE_NAME, useNewUrlParser: true}, (error) =>{
             if (error) {
                 throw error;
             }
-
-            database = client.db(DATABASE_NAME);
-
-            collection = database.collection("disciplinas");
-
             console.log("Connected to `" + DATABASE_NAME + "`!");
         });
     }
