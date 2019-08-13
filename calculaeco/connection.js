3ace7a7fa07e93e2693f271c7eb57b6f87764c04
Moon Koala <moonkoala23@gmail.com>
2019-08-13 11:47:50 -0300

    Database change

--- calculaeco/connection.js
+++ calculaeco/connection.js
@@ -2,11 +2,11 @@ module.exports = {
     connect: function () {
         
         const MongoClient = require("mongodb").MongoClient;
-        const mongoose = require('mongoose').mongoose;
+        // const mongoose = require('mongoose');
         const ObjectId = require("mongodb").ObjectID;
         
-        let CONNECTION_URL = "mongodb+srv://public:HxnpHEg3OnxJgDSt@cluster0-yfisq.gcp.mongodb.net/test?retryWrites=true&w=majority";
-        CONNECTION_URL = process.env.MONGODB_URI;
+        // let CONNECTION_URL = "mongodb+srv://public:HxnpHEg3OnxJgDSt@cluster0-yfisq.gcp.mongodb.net/test?retryWrites=true&w=majority";
+        let CONNECTION_URL = process.env.MONGODB_URI || "mongodb+srv://public:HxnpHEg3OnxJgDSt@cluster0-yfisq.gcp.mongodb.net/test?retryWrites=true&w=majority";
         const DATABASE_NAME = "bdcalculaeco";
 
         MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
