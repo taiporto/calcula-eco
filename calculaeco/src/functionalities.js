265b8fa49d2709374194a86e47fe2a7b73460d9c
liaporto <moonkoala23@gmail.com>
2020-07-01 17:40:35 -0300

    correct background-image on 3-column calculator layout

--- calculaeco/src/functionalities.js
+++ calculaeco/src/functionalities.js
@@ -1,45 +1,35 @@
-module.exports = {  
-    
-    isEmpty: function(obj){
-        return Object.keys(obj).length === 0;
-    },
+module.exports = {
+  isEmpty: function (obj) {
+    return Object.keys(obj).length === 0;
+  },
 
-    //TIRAR MATERIAS Q A PESSOA N COLOCOU NADA (IMPLEMENTAR FUNÇÃO DE EXCLUIR MATÉRIAS)
-    calculoCR: function (notasArray, creditosArray) {
+  // TODO: Tirar matérias em que o usuário não inseriu nenhuma nota
+  calculoCR: function (notasArray, creditosArray) {
+    let sumCredito = 0;
+    let sumNotas = 0;
 
-        let sumCredito = 0;
-        let sumNotas = 0;
-
-        for (let j = 0; j < creditosArray.length; j++) {
-
-            sumCredito += creditosArray[j];
-        }
-
-        console.log(sumCredito);
-
-        for (let i = 0; i < notasArray.length; i++) {
-            
-            let nota = notasArray[i] * creditosArray[i];
-            
-            sumNotas += nota;
-        }
-        
-        console.log(sumNotas);
-
-        let cr = (sumNotas / sumCredito);
-
-        console.log(cr);
-
-        cr = cr.toFixed(1);
-
-        let crFinal = parseFloat(cr);
-
-        return crFinal;
-
+    for (let j = 0; j < creditosArray.length; j++) {
+      sumCredito += creditosArray[j];
     }
-}
 
+    console.log(sumCredito);
 
+    for (let i = 0; i < notasArray.length; i++) {
+      let nota = notasArray[i] * creditosArray[i];
 
+      sumNotas += nota;
+    }
 
+    console.log(sumNotas);
 
+    let cr = sumNotas / sumCredito;
+
+    console.log(cr);
+
+    cr = cr.toFixed(1);
+
+    let crFinal = parseFloat(cr);
+
+    return crFinal;
+  },
+};
